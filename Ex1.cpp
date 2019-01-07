6467f9a0a944ef6eb7c08d1fba4c67c31022087b
آرش رضایی <arashsoft@outlook.com>
2019-01-07 20:51:53 +0330

    Create Ex1.cpp

---
+++ Ex1.cpp
@@ -0,0 +1,43 @@
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <windows.h>  
+
+using namespace std;    
+
+#include <stdio.h>
+
+
+	int main()
+	{
+		for (;;) {
+
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bMahdi is good   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmAhdi is good   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmaHdi is good   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmahDi is good   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmahdI is good   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmahdi Is good   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmahdi iS good   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmahdi is Good   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmahdi is gOod   " << std::flush;
+			Sleep(100);
+			std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmahdi is goOd   " << std::flush;
+			Sleep(100); 
+				std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bmahdi is gooD   " << std::flush;
+			Sleep(100);
+
+		}
+	}
+
+	
